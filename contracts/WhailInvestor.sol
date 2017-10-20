e5cb37340afd354cadb9c9e89453609d6dad0b69
xpeper <xpepermint@gmail.com>
2017-10-20 22:16:59 +0200

    accept deposits

--- contracts/WhailInvestor.sol
+++ contracts/WhailInvestor.sol
@@ -1,4 +1,46 @@
 pragma solidity ^0.4.13;
 
 contract WhailInvestor {
+
+  /**
+   * Contract stages.
+   */
+  enum Stages {
+    AcceptingDeposits
+  }
+
+  /**
+   * Current contract stage.
+   */
+  Stages public stage = Stages.AcceptingDeposits;
+
+  /**
+   * Depesit amounts per address.
+   */
+  mapping (address => uint256) public deposits;
+
+  /**
+   * Total amount ever received.
+   */
+  uint256 public totalDepositsAmount;
+
+  /**
+   * Handles a deposit from ethereum wallets.
+   */
+  function acceptDepositOf(address _from, uint _amount) internal {
+    require(_amount > 0);
+
+    totalDepositsAmount += _amount;
+    deposits[_from] += _amount;
+  }
+
+  /**
+   * Accepts deposits, withdraws deposits or transfer tokens.
+   */
+  function() payable {
+    if (stage == Stages.AcceptingDeposits) {
+      acceptDepositOf(msg.sender, msg.value);
+    }
+  }
+
 }
--- test/whail_investor.js
+++ test/whail_investor.js
@@ -1,6 +1,30 @@
 const WhailInvestor = artifacts.require("../contracts/WhailInvestor.sol");
 
 contract('WhailInvestor', (accounts) => {
-  let [owner, seller, token, participant] = accounts;
+
+  const [owner, seller, token, participant] = accounts;
+
+  describe('#acceptDepositOf()', () => {
+
+    it("accepts deposits", async () => {
+      const amount = 1000;
+      const contract = await WhailInvestor.new();
+
+      web3.eth.sendTransaction({
+        from: participant,
+        to: contract.address,
+        value: amount,
+      });
+
+      const contractBalance = web3.eth.getBalance(contract.address);
+      const totalDepositsAmount = await contract.totalDepositsAmount.call();
+      const participantDepositBalance = await contract.deposits.call(participant);
+
+      assert.equal(contractBalance.toNumber(), amount);
+      assert.equal(totalDepositsAmount.toNumber(), amount);
+      assert.equal(participantDepositBalance.toNumber(), amount);
+    });
+
+  });
 
 });
